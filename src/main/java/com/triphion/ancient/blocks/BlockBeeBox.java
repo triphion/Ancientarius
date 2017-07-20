ec6a9f0d7f3f5756cde6ba729d79ae763ee334c1
triphion <30317346+triphion@users.noreply.github.com>
2017-07-20 21:18:53 +0200

    Add files via upload

---
+++ src/main/java/com/triphion/ancient/blocks/BlockBeeBox.java
@@ -0,0 +1,55 @@
+package com.triphion.ancient.blocks;
+
+import com.triphion.ancient.AncientMod;
+import com.triphion.ancient.Reference;
+import com.triphion.ancient.util.Utils;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BlockBeeBox extends Block {
+
+	public BlockBeeBox(String unlocalizedName, String registryName) {
+        super(Material.ROCK);
+		this.setUnlocalizedName(unlocalizedName);
+		this.setRegistryName(new ResourceLocation(Reference.MODID, registryName));
+		this.setHardness(2);
+		this.setResistance(2);
+		this.setHarvestLevel("pickaxe", 0);
+
+	}
+    
+	@SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess worldIn, BlockPos pos, EnumFacing side) {
+        return false;
+    }
+
+    @Override
+    public boolean isBlockNormalCube(IBlockState blockState) {
+        return false;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState blockState) {
+        return false;
+    }
+
+}
